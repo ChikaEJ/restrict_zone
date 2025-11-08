9147420955875bf152a483db2105fdb1e24cb58f
nesuraisyn04 <nesuraisyn04@gmail.com>
2025-11-08 11:54:09 +0500

    feat: create app and core folders and config.py

---
+++ app/core/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    ...
+
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()
